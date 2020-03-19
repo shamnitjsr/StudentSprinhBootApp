307e2c5c9006e938e34a4a187085237040c114e3
SH14732 <shamnitjsr@gmail.com>
2020-03-19 16:00:55 +0530

    1st_commit

---
+++ src/test/java/com/infotech/app/StudentSpringBootApp/StudentSpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotech.app.StudentSpringBootApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentSpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
